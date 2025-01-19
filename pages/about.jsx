f284be4d6496972acc4ce1c4b39a74f523d3502a
Anhadpreet Singh <94036563+Anhad928@users.noreply.github.com>
2025-01-19 13:52:29 -0700

    Refactor AboutPage and SettingsPage components and styles

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,7 +1,14 @@
 const AboutPage = () => {
     return (
       <>
-        <h3>A Little Bit About Me</h3>
+        <h3>I am a Full Stack Software Developer and AI/ML enthusiast with a strong foundation in Python,
+            Java, JavaScript, and TypeScript, complemented by hands-on experience with frameworks such as 
+             Next.js, React and Spring Boot. My background includes leading Agile teams, implementing 
+            secure authentication and database solutions, and optimizing complex workflows for improved 
+            performance and usability. Leveraging my passion for machine learning, I’ve integrated AI-driven 
+            features into applications—most notably in a resume builder platform—to deliver intelligent, user-
+            centric experiences. I’m driven by a commitment to building scalable, innovative solutions that 
+            blend robust back-end architecture with intuitive front-end interfaces, and I thrive in collaborative environments where I can contribute, learn, and keep pushing the boundaries of technology.</h3>
       </>
     );
   };
